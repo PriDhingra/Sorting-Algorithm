98228f49c47326ddde6f66de02266684c2e53da5
Prince Dhingra <princedhingra25@gmail.com>
2022-03-19 11:27:43 -0400

    Create RotatedArray.cpp

---
+++ RotatedArray.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void printArray(vector<int> &vect) {
+    for(int i = 0;i < vect.size();i++) {
+        cout<<vect[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void rotateArray(vector<int> &vect, int k) {
+    vector<int> temp(vect.size());
+
+    for(int i = 0; i < vect.size();i++) {
+        temp[(i + k) % vect.size()] = vect[i];
+    }
+
+    vect = temp;
+}
+
+int main() {
+    vector<int> vect;
+
+    vect.push_back(1);
+    vect.push_back(2);
+    vect.push_back(3);
+    vect.push_back(4);
+    vect.push_back(5);
+    vect.push_back(6);
+
+    rotateArray(vect, 3);
+    cout<<"Rotated Array : ";
+    printArray(vect);
+
+    return 0;
+}
